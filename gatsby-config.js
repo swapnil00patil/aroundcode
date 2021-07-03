be1dd0f4481ef913db16cf657cb0af41bf22a190
swapnil00patil <swapnil00patil@gmail.com>
2021-07-03 11:32:03 +0800

    fix: remove gtag id from code

--- gatsby-config.js
+++ gatsby-config.js
@@ -95,7 +95,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         trackingIds: [
-          "G-GERT6V31X2", // Google Analytics / GA
+          process.env.GTAG_ID
         ]
       },
     },
